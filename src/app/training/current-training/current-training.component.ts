c523cfc328b0b453c1b4e05c73a6dc124894d4d4
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-17 16:54:42 +0300

    haberleşme sağlandı

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { StopTrainingComponent } from '../stop-training/stop-training.component';
 
@@ -8,11 +8,16 @@ import { StopTrainingComponent } from '../stop-training/stop-training.component'
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter<any>();
+
   progress = 0;
   timer: any;
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.startOrResumeTimer();
+  }
+  startOrResumeTimer() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress >= 100) {
@@ -28,7 +33,9 @@ export class CurrentTrainingComponent implements OnInit {
       }
     });
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if (this.trainingExit) {
+        this.trainingExit.emit();
+      }
     });
   }
 }
