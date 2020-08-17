35d981d08c3775d1b2f0d11d3cefedd28f6f0b37
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-17 16:14:30 +0300

    progress durdurma event eklendi

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -7,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
+  timer: any;
   constructor() { }
 
   ngOnInit() {
-    setInterval(() => {
+    this.timer = setInterval(() => {
       this.progress = this.progress + 5;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
     }, 1000);
   }
-
+  onStop(): void {
+    clearInterval(this.timer);
+  }
 }
