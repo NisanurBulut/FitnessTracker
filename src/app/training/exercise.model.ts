e32fcb8f10736b3babcfefdc64cd9c38773fa47d
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-18 12:34:37 +0300

    exersiz model ve servis yazıldı

---
+++ src/app/training/exercise.model.ts
@@ -0,0 +1,9 @@
+export interface Exercise {
+    id: string;
+    name: string;
+    duration: number;
+    calories: number;
+    date?: Date;
+    state?: 'completed' | 'cancelled' | null;
+
+}
---
+++ src/app/training/training.service.ts
@@ -0,0 +1,11 @@
+import { Exercise } from './exercise.model';
+
+export class TrainingService {
+    availableExercises: Exercise[] = [
+        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+        { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 }
+    ];
+}
