7ce4645cc45cb682543f59a1a4588005cc717778
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-18 12:58:28 +0300

    aktif exercise kontrolü eklendi

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -8,8 +8,11 @@ export class TrainingService {
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 }
     ];
-
+    private runningExercise: Exercise;
     getAvailableExercices() {
         return this.availableExercises.slice();
+    }
+    startExercise(selectId: string) {
+        this.runningExercise = this.availableExercises.find(a => a.id === selectId);
     }
 }
