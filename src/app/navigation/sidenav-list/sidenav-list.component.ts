0cd8d6b9a24f4061a1bad86162f48fb5d5840ea4
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-18 11:05:08 +0300

    menulerde oturum açma kontrolleri eklendi

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSideNav = new EventEmitter<void>();
-  constructor() { }
+  isAuth: boolean;
+  authSubscription: Subscription;
+  constructor(private as: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.as.authChange.subscribe((authStatus) => {
+      this.isAuth = authStatus;
+    });
+  }
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
   onClose() {
     this.closeSideNav.emit();
