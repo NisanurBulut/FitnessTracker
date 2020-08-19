36024fe2d04b4dbd43704f78e5c066619452916b
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-19 23:02:01 +0300

    login işlemine spinner eklendi

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm, ɵNgNoValidate } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { UIService } from 'src/app/shared/ui-service';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   maxDate;
-  constructor(private as: AuthService) { }
+  isLoading = false;
+  private loadingSub: Subscription;
+
+  constructor(private as: AuthService, private uis: UIService) { }
 
   ngOnInit() {
+    this.loadingSub = this.uis.loadingStateSubject.subscribe(isLoad => {
+      this.isLoading = isLoad;
+    });
+
     this.maxDate = new Date();
     this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+  ngOnDestroy() {
+    this.loadingSub.unsubscribe();
   }
   onSubmit(f: NgForm) {
     this.as.registerUser({
