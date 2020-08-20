95fb54c712f4ef62625835512430fde79ea17e2a
NisanurBulut <nisanurrunasin@gmail.com>
2020-08-20 16:02:52 +0300

    datatable state düzenlemesi yapıldı

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,34 +1,31 @@
-import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../Training.service';
-import { Subscription } from 'rxjs';
-
+import * as fromTraining from '../training.reducer';
+import { Store } from '@ngrx/store';
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'calories', 'duration', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
-  private exChangedSuscription: Subscription;
+
 
   @ViewChild(MatSort, { static: false }) tsort: MatSort;
   @ViewChild(MatPaginator, { static: false }) tPaginator: MatPaginator;
-  constructor(private ts: TrainingService) { }
+  constructor(private ts: TrainingService, private store: Store<fromTraining.State>) { }
 
   ngOnInit() {
-    this.exChangedSuscription = this.ts.finishedExercisesChange.subscribe((exerRes: Exercise[]) => {
-      this.dataSource.data = exerRes;
-    });
+    this.store.select(fromTraining.getFinishedExercises)
+      .subscribe((exerRes: Exercise[]) => {
+        this.dataSource.data = exerRes;
+      });
     this.ts.fetchComplatedOrCancelledExercises();
   }
-  ngOnDestroy() {
-    if (this.exChangedSuscription) {
-      this.exChangedSuscription.unsubscribe();
-    }
-  }
+
   ngAfterViewInit() {
     this.dataSource.sort = this.tsort;
     this.dataSource.paginator = this.tPaginator;
