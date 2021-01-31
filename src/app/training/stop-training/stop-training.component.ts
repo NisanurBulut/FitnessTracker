0108eccf2cef5d7881977f199b757f1517218d5c
NisanurBulut <nbulut@sarplojistik.com>
2021-01-31 14:09:58 +0300

    restyled header

--- src/app/training/stop-training/stop-training.component.ts
+++ src/app/training/stop-training/stop-training.component.ts
@@ -3,8 +3,8 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
     selector: 'app-stop-training',
-    template: '<h1 mat-dialog-title>Emin Misiniz ?</h1>'
-        + '<mat-dialog-content>zaten {{passedData.progress}} % tamamladın.</mat-dialog-content>'
+    template: '<h1 mat-dialog-title>Are you sure ?</h1>'
+        + '<mat-dialog-content>Already done {{passedData.progress}} %.</mat-dialog-content>'
         + '<mat-dialog-actions>'
         + '<button mat-button [mat-dialog-close]="true"> Evet </button>'
         + '<button mat-button [mat-dialog-close]="false"> Hayır </button>'
